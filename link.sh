30943394a184c1eb6443c3bf8892d17584890851
wastedsenseofvalue <nakajima.k0301@gmail.com>
2023-09-10 09:52:26 +0900

    update

--- link.sh
+++ link.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DOT_DIR="$HOME/dotfiles"
+
+# symlink
 ln -sv $DOT_DIR/bashrc $HOME/.bashrc
 ln -sv $DOT_DIR/gitconfig $HOME/.gitconfig
 ln -sv $DOT_DIR/gitconfig_global $HOME/.gitconfig_global
@@ -8,4 +10,8 @@ ln -sv $DOT_DIR/inputrc $HOME/.inputrc
 ln -sv $DOT_DIR/tmux.conf $HOME/.tmux.conf
 ln -sv $DOT_DIR/starship.toml $HOME/.config/starship.toml
 ln -sv $DOT_DIR/vimrc $HOME/.vimrc
+
+# create local files
+touch $HOME/.gitconfig.user && echo "Please edit the ~/.gitconfig.user"
+touch $HOME/.bashrc.local && echo "Please edit the ~/.bashrc.local"
 
