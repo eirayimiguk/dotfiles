d6c2c0bc26d4f8ae37d28be83b4d566b1c46f937
eirayimiguk <nakajima.k0301@gmail.com>
2024-03-07 17:07:21 +0900

    update

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,9 +1,37 @@
-vim.wo.number = true
+-- Options
+local opt = vim.opt
+opt.textwidth = 120
+opt.scrolloff = 7
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.number = true
+opt.relativenumber = true
+opt.smartindent = true
+opt.laststatus = 3
+opt.hidden = true
+opt.undofile = true
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
+-- Color
+opt.termguicolors = true
+opt.background = 'dark'
 
-vim.o.ignorecase = true
-vim.o.smartcase = true
+-- Keybind
+local silent = { silent = true, noremap = true }
+local map = vim.api.nvim_set_keymap
+
+-- Exit insert mode (jj)
+map('i', 'jj', '<Esc>', silent)
+
+-- Window movement
+map('n', '<c-h>', '<c-w>h', silent)
+map('n', '<c-j>', '<c-w>j', silent)
+map('n', '<c-k>', '<c-w>k', silent)
+map('n', '<c-l>', '<c-w>l', silent)
+
+-- Tab movement
+map('n', '<c-Left>', '<cmd>tabpre<cr>', silent)
+map('n', '<c-Right>', '<cmd>tabnext<cr>', silent)
 
